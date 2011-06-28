b8bec409e19b6de7bebe05774ba049b32067c471
Jonathan "Duke" Leto <jonathan@leto.net>
2011-06-28 14:49:25 -0700

    Add a stub for Algorithm::Soundex in perl 6

---
+++ lib/Algorithm/Soundex.pm
@@ -0,0 +1,28 @@
+use v6;
+
+class Algorithm::Soundex {
+
+}
+=begin pod
+
+=head1 NAME
+
+Algorithm::Soundex - The Soundex Algorithm
+
+=head1 DESCRIPTION
+
+The very simple American Soundex algorithm, implemented in Perl 6.
+
+=head1 SYNOPSIS
+
+=begin code
+
+INSERT CODE HERE
+
+=end code
+
+=head1 AUTHOR
+
+Jonathan "Duke" Leto - L<jonathan@leto.net>
+
+=end pod
---
+++ t/00-basic.t
@@ -0,0 +1,5 @@
+use v6;
+use Test;
+use Algorithm::Soundex;
+
+pass("Loaded Algorithm::Soundex");
